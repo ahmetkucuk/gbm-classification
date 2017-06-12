08e6a1981750c589cc36289f6f75caebc6183a95
ahmetkucuk <ahmetkucuk92@gmail.com>
2017-06-12 06:45:50 -0400

    fix data pipeline

--- tf_data_pipeline.py
+++ tf_data_pipeline.py
@@ -150,7 +150,7 @@ class DataPipeline(object):
 		key, value = reader.read(image_queue)
 
 		my_img = tf.image.decode_jpeg(value)
-		my_img.set_shape([1024, 1024, 3])
+		my_img.set_shape([256, 256, 3])
 		my_img = tf.image.per_image_standardization(my_img)
 
 		self.batched_image = tf.train.batch([my_img], batch_size=batch_size)
